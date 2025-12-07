cb4fe87dbf0be101dba11cda7e1ab0a0b497d942
Minh Khánh <khanh.lm263@gmail.com>
2025-12-07 20:58:39 +0700

    cau truc code

---
+++ src/_Data/models/PlayerStateData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioGame.src._Data.models
+{
+    internal class PlayerStateData
+    {
+    }
+}
---
+++ src/_Entities/items/Coin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioGame.src._Entities.items
+{
+    internal class Coin
+    {
+    }
+}
