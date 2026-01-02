c67cce28422f4388fb8696b80d6b44572c101b45
Lê Hoàng Hiệp <megpjhu@gmail.com>
2026-01-02 23:39:51 +0700

    Update GameState.cs

--- Core/GameState.cs
+++ Core/GameState.cs
@@ -8,56 +8,12 @@ namespace MarioGame.Core
 {
     internal class GameState
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-        StartScreen,
+        StartMenu,
+        LevelSelect,
         Playing,
         Paused,
-        LevelComplete,
         GameOver,
-        Settings,
-        LevelSelect
-    }
-
-    public enum PlayerState
-    {
-        Idle,
-        Running,
-        Jumping,
-        Falling,
-        Dead
-    }
-
-    public enum PowerUpState
-    {
-        Small,
-        Big,
-        Fire
-    }
-
-    public enum EnemyState
-    {
-        Walking,
-        Shell,
-        Sliding,
-        Dead
-    }
-
-    public enum CameraMode
-    {
-        FollowPlayer,
-        AutoScroll,
-        Fixed
-=======
->>>>>>> parent of 03770b5 (utf8)
-    }
-=======
-    }
->>>>>>> parent of 03770b5 (utf8)
-=======
-=======
->>>>>>> parent of 03770b5 (utf8)
+        Victory,
+        Settings
     }
 }
